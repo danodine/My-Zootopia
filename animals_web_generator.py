90c6b274a8ebdd8906dffaa27cbed0e0cca1b8bd
David Nodine <davidnodine@gmail.com>
2025-03-26 16:10:07 +0100

    impruved syles and changed the saving of file to create a new file

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -17,14 +17,16 @@ def create_animal_string(animals_data):
     for animal in animals_data:
         output += '<li class="cards__item">'
         output += '<div class ="card__title">'
-        output += f"{animal['name']}<br/>\n"
+        output += f"{animal['name']}\n"
         output += '</div>'
         output += '<p class ="card__text">'
-        output += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>\n"
+        output += '<ul>'
+        output += f"<li><strong>Diet:</strong> {animal['characteristics']['diet']}</li>\n"
         for location in animal['locations']:
-            output += f"<strong>Location:</strong> {location}<br/>\n"
+            output += f"<li><strong>Location:</strong> {location}</li>\n"
         if 'type' in animal['characteristics']:
-            output += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>\n"
+            output += f"<li><strong>Type:</strong> {animal['characteristics']['type']}</li>\n"
+        output += '</ul>'
         output += '</p>'
         output += '</li>'
     return output
@@ -40,7 +42,7 @@ def main():
     html_page = read_html('animals_template.html')
     output = create_animal_string(animals_data)
     new_html_page = html_page.replace('__REPLACE_ANIMALS_INFO__', output)
-    write_new_html_page(new_html_page, 'animals_template.html')
+    write_new_html_page(new_html_page, 'animals.html')
 
 
 if __name__ == '__main__':
