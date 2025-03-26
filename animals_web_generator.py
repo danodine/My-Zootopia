c3edb630f1b7478755c7a2f7cf4c367af71f13ce
David Nodine <davidnodine@gmail.com>
2025-03-26 15:23:59 +0100

    creating logic to print the data from animals

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,7 +1,20 @@
+import json
+
+def load_data(file_path):
+  """ Loads a JSON file """
+  with open(file_path, "r") as handle:
+    return json.load(handle)
 
 
 def main():
-    pass
+    animals_data = load_data('animals_data.json')
+    for animal in animals_data:
+        print(f"Name: {animal['name']}")
+        print(f"Diet: {animal['characteristics']['diet']}")
+        for location in animal['locations']:
+            print(f"Location: {location}")
+        if 'type' in animal['characteristics']:
+            print(f"Type: {animal['characteristics']['type']}")
 
 
 if __name__ == '__main__':
