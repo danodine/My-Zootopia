afb615a68a55809429010fff4b7a27858e6cb603
David Nodine <davidnodine@gmail.com>
2025-03-26 16:45:06 +0100

    created a filter for skin type

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -12,6 +12,7 @@ def read_html(file_path):
     with open(file_path, "r") as handle:
         return handle.read()
 
+
 def create_animal_string(animals_data):
     output = ''
     for animal in animals_data:
@@ -31,16 +32,53 @@ def create_animal_string(animals_data):
         output += '</li>'
     return output
 
+
+def filter_animals(animals_data, animal_skin_type):
+    filtered_animals = [item for item in animals_data if item.get("characteristics", {}).get("skin_type").strip().lower() == animal_skin_type]
+    output = create_animal_string(filtered_animals)
+    return output
+
+
 def write_new_html_page(new_html_page, file_path):
     """ writes a html file """
     with open(file_path, "w") as handle:
         return handle.write(new_html_page)
 
 
+def get_animals_skin(animals_data):
+    skin_types = []
+    print('the skin types are:')
+    for animal in animals_data:
+        if 'skin_type' in animal['characteristics']:
+            if animal['characteristics']['skin_type'].strip().lower() not in skin_types:
+                skin_types.append(animal['characteristics']['skin_type'].strip().lower())
+                print(animal['characteristics']['skin_type'])
+    while True:
+        user_selection = input('By what skin type wold you like to filter: ')
+        if user_selection in skin_types:
+            return user_selection
+        else:
+            print('you must enter a valid skin type')
+
+
 def main():
     animals_data = load_data('animals_data.json')
+    input_valid = False
+    animal_skin_type = ''
+    while not input_valid:
+        new_game =  input('Do you want to filter by skin type Y/N: ').strip().lower()
+        if new_game == 'y':
+            animal_skin_type = get_animals_skin(animals_data)
+            input_valid = True
+        elif new_game == 'n':
+            input_valid = True
+        else:
+            print("Invalid input. Please enter Y or N.")
     html_page = read_html('animals_template.html')
-    output = create_animal_string(animals_data)
+    if animal_skin_type != '':
+        output = filter_animals(animals_data, animal_skin_type)
+    else:
+        output = create_animal_string(animals_data)
     new_html_page = html_page.replace('__REPLACE_ANIMALS_INFO__', output)
     write_new_html_page(new_html_page, 'animals.html')
 
