e5d2a7cfa96a4f9b04d2342efaed8525a2ec15c3
David Nodine <davidnodine@gmail.com>
2025-03-26 15:41:32 +0100

    writing animals to html as a string

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,20 +1,40 @@
 import json
 
+
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
+
+
+def read_html(file_path):
+    """ Loads a html file """
+    with open(file_path, "r") as handle:
+        return handle.read()
+
+def create_animal_string(animals_data):
+    output = ''
+    for animal in animals_data:
+        output += f"Name: {animal['name']}\n"
+        output += f"Diet: {animal['characteristics']['diet']}\n"
+        for location in animal['locations']:
+            output += f"Location: {location}\n"
+        if 'type' in animal['characteristics']:
+            output += f"Type: {animal['characteristics']['type']}\n"
+    return output
+
+def write_new_html_page(new_html_page, file_path):
+    """ writes a html file """
+    with open(file_path, "w") as handle:
+        return handle.write(new_html_page)
 
 
 def main():
     animals_data = load_data('animals_data.json')
-    for animal in animals_data:
-        print(f"Name: {animal['name']}")
-        print(f"Diet: {animal['characteristics']['diet']}")
-        for location in animal['locations']:
-            print(f"Location: {location}")
-        if 'type' in animal['characteristics']:
-            print(f"Type: {animal['characteristics']['type']}")
+    html_page = read_html('animals_template.html')
+    output = create_animal_string(animals_data)
+    new_html_page = html_page.replace('__REPLACE_ANIMALS_INFO__', output)
+    write_new_html_page(new_html_page, 'animals_template.html')
 
 
 if __name__ == '__main__':
